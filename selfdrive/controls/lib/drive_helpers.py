4f9076c7781126f7f9e114c24365ad779a89fecc
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-03-30 21:40:45 +0900

    Update drive_helpers.py

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -114,13 +114,13 @@ def get_lag_adjusted_curvature(CP, v_ego, psis, curvatures, curvature_rates):
   psi = interp(delay, T_IDXS[:CONTROL_N], psis)
   desired_curvature_rate = curvature_rates[0]
   
+  v_ego = max(v_ego, 1)
   # MPC can plan to turn the wheel and turn back before t_delay. This means
   # in high delay cases some corrections never even get commanded. So just use
   # psi to calculate a simple linearization of desired curvature
   curvature_diff_from_psi = psi / (max(v_ego, 1e-1) * delay) - current_curvature
   desired_curvature = current_curvature + 2 * curvature_diff_from_psi
 
-  v_ego = max(v_ego, 0.1)  
   LATERAL_JERK = interp(v_ego, MAX_LATERAL_JERK_SPEEDS, MAX_LATERAL_JERKS)
   #max_curvature_rate = MAX_LATERAL_JERK / (v_ego**2)
   max_curvature_rate = LATERAL_JERK / (v_ego**2)
